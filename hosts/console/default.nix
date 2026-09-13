2a455fe752c1fed3db677899ef15f88bbeba12db
charlie <chwiegand@proton.me>
2026-09-13 16:21:32 +0200

    console: waydroid

--- hosts/console/default.nix
+++ hosts/console/default.nix
@@ -70,7 +70,7 @@ in
 
   virtualisation.waydroid.enable = true;
   boot.kernelModules = [ "binder_linux" ];
-  networking.nftables.enable = false;
+  networking.nftables.enable = true;
 
   boot.loader = {
     systemd-boot.enable = true;
